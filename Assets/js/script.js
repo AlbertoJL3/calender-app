4a7b98f3468b582b0f99810d7b3856f6071bbf3f
Alberto Lopez <aj@albertos-mbp.lan>
2022-11-05 12:51:24 -0400

    styled timeblocks

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,8 @@
 var date = moment().format("ddd MMM Do, YYYY");
 $('#currentDay').text(date);
 
-
-
 setInterval(function () {
-    var time = moment().format("hh:mm:ss");
+    var time = moment().format('LT');
     $('#currentTime').text(time);
 }, 1000);
+
