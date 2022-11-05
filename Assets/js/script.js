cc527488d3bd6d1a3bbb36985863bd9f3712237e
Alberto Lopez <aj@albertos-mbp.lan>
2022-11-05 12:25:57 -0400

    added moment.js for displaying date

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,25 +1,9 @@
-var shoppingFormEl = $('#shopping-form');
-var shoppingListEl = $('#shopping-list');
+var date = moment().format("ddd MMM Do, YYYY");
+$('#currentDay').text(date);
 
-// create function to handle form submission
-function handleFormSubmit(event) {
-  event.preventDefault();
 
-  // select form element by its `name` attribute and get its value
-  var shoppingItem = $('input[name="shopping-input"]').val();
 
-  // if there's nothing in the form entered, don't print to the page
-  if (!shoppingItem) {
-    console.log('No shopping item filled out in form!');
-    return;
-  }
-
-  // print to the page
-  shoppingListEl.append('<li>' + shoppingItem + '</li>');
-
-  // clear the form input element
-  $('input[name="shopping-input"]').val('');
-}
-
-// Create a submit event listener on the form element
-shoppingFormEl.on('submit', handleFormSubmit);
+setInterval(function () {
+    var time = moment().format("hh:mm:ss");
+    $('#currentTime').text(time);
+}, 1000);
